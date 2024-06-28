215ed03869ba71a84e142c6a3db5b25800ec8edb
Eugene Borovikov <eugene.b@spectral-team.com>
2024-06-28 12:35:16 +0400

    add CopilotChat

---
+++ lua/plugins/CopilotChat.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false, -- Enable debugging
+    },
+  },
+}
