944f92555ff133ba1ba24f0346eb67e2532eac6a
Eugene Borovikov <eugene.b@spectral-team.com>
2024-07-01 16:43:59 +0400

    update keybindings for copilot

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,11 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
--- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
----@type LazySpec
 return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
@@ -43,10 +35,19 @@ return {
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
       -- first key is the mode
+      i = {
+        ["<F12>"] = { 'copilot#Accept("\\<CR>")',
+          desc = "Accept copilot suggestion",
+          expr = true,
+          replace_keycodes = false,
+        },
+      },
+
       n = {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs with `H` and `L`
+        --
         L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
