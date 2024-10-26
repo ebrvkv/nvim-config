2bfe7873635557835eb6ca7287cb6f56ddb4391e
Evgeny Borovikov <borovikov.ea@gmail.com>
2024-10-26 14:35:20 +0400

    use bzl as ls for bazel and getcwd for launch.json

--- lua/polish.lua
+++ lua/polish.lua
@@ -13,7 +13,9 @@ vim.filetype.add {
 vim.api.nvim_create_autocmd("DirChanged", {
   desc = "Run commands in DirChanged event",
   callback = function()
-	  require('dap.ext.vscode').load_launchjs('./launch.json', { delve = { 'delve', 'go' } })
-	  require('dap.ext.vscode').load_launchjs()
+	  require('dap.ext.vscode').load_launchjs(
+	    vim.fn.getcwd()+'/launch.json',
+	    { delve = { 'delve', 'go' } }
+	  )
   end,
 })
